e1fe3eee3ea34205c518b3fc6b3c6b2788ed6afe
Arseniy <arseniysharnin@gmail.com>
2026-02-02 17:54:26 +0700

    Практическая работа №7

--- js/main.js
+++ js/main.js
@@ -4,10 +4,9 @@ let app = new Vue({
         product: "Socks", // Место для хранения данных в Vue
         brand: 'Vue Mastery',
         description: "A pair of warm, fuzzy socks", // Практическая работа №1 (добавление ключа к экземпляру)
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        selectedVariant: 0,
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks", // Практическая работа №2 (добавление ссылки на страницу)
-        inStock: false,
         onSale: true, // Практическая работа №3
         details: ['80% cotton', '20% polyester', 'Gender-neutral'], // массив details
         variants: [
@@ -15,11 +14,13 @@ let app = new Vue({
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10,
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0,
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
@@ -29,18 +30,34 @@ let app = new Vue({
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index
         },
         subToCart(){
             if (this.cart > 0) {
                 this.cart -= 1
             } // Практическая работа №5 (Добавление кнопки вычета)
         }
+        
     },
     computed: {
         title() {
             return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if (this.onSale) {
+                return `Распродажа! ${this.brand} ${this.product}`
+            }
+            else {
+                return `${this.brand} ${this.product} - распродажа не проводится`
+            }
+            return this.daw
         }
     }
 
