0f90eb80e5fb75b8b1bf860a90ef60bdbf5dc6a0
Arseniy <arseniysharnin@gmail.com>
2026-02-02 17:25:04 +0700

    prakticheskaya 6

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ let app = new Vue({
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks", // Практическая работа №2 (добавление ссылки на страницу)
-        inStock: true,
+        inStock: false,
         onSale: true, // Практическая работа №3
         details: ['80% cotton', '20% polyester', 'Gender-neutral'], // массив details
         variants: [
