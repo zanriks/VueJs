8db108ab39bab09d1a00983c18e92d19272a780d
Арсений Шарнин <gr434_shaaal2@lab-211-8.tomtit-tomsk.ru>
2026-02-03 11:46:26 +0700

    commit 10.8

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+Vue.component('product-details', {
+    props: {
+        details: {
+            type: Number,
+            required: true
+        }
+    },
+    template: `
+    <ul class="product-details">
+        <li v-for="(detail, index) in details" :key="index" class="detail-item">
+            <span class="detail-text">{{ detail }}</span>
+        </li>
+    </ul>`
+})
+
 Vue.component('product', {
     props: {
         premium: {
@@ -15,12 +30,9 @@ Vue.component('product', {
             <p> {{ description }}</p>
             <a :href="link">More products like this</a>
             <p v-if="inStock">In Stock</p>
-            <!-- Практическая работа №6 (перечеркивание текста) -->
             <p v-else :class="{outOfStock: !inStock}" :style="{textDecoration: 'line-through'}">Out of Stock</p>
             <span>{{ sale }}</span>
-            <ul>
-                <li v-for="detail in details">{{ detail }}</li>
-            </ul>
+            <product-details :details="details"></product-details>            
             <p>Shipping: {{ shipping }}</p>
             <div class="color-box"
                 v-for="(variant, index) in variants"
@@ -31,27 +43,25 @@ Vue.component('product', {
             <div v-for="size in sizes">
                 <p> {{ size }} </p>
             </div>
-            <div class="cart">
-                <p>Cart({{ cart }})</p>
-            </div>
-            <button v-on:click="addToCart"
+            <button 
+                v-on:click="addToCart"
                 :disabled="!inStock"
                 :class="{disabledButton: !inStock}"
-                >Add to cart</button>
+            >Add to cart</button>
             <button v-on:click="subToCart">Sub to cart</button>
         </div>
     </div>
-`,
+    `,
     data() {
         return {
             product: "Socks",
             brand: 'Vue Mastery',
-            description: "A pair of warm, fuzzy socks", // Практическая работа №1 (добавление ключа к экземпляру)
+            description: "A pair of warm, fuzzy socks",
             selectedVariant: 0,
             altText: "A pair of socks",
-            link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks", // Практическая работа №2 (добавление ссылки на страницу)
-            onSale: true, // Практическая работа №3
-            details: ['80% cotton', '20% polyester', 'Gender-neutral'], // массив details
+            link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+            onSale: true,
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
             variants: [
                 {
                     variantId: 2234,
@@ -67,20 +77,17 @@ Vue.component('product', {
                 }
             ],
             sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-            cart: 0,
         }
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            this.$emit('add-to-cart')
         },
         updateProduct(index) {
             this.selectedVariant = index
         },
         subToCart(){
-            if (this.cart > 0) {
-                this.cart -= 1
-            } // Практическая работа №5 (Добавление кнопки вычета)
+            this.$emit('sub-from-cart')  // ✅ Эмитим событие
         }
     },
     computed: {
@@ -111,14 +118,21 @@ Vue.component('product', {
     }
 })
 
-Vue.component('product-details', {
-    template: ``
-})
-
 let app = new Vue({
     el: '#app',
     data: {
-        premium: true
+        premium: true,
+        cart: 0,
+    },
+    methods: {
+        updateCart() {
+            this.cart += 1;
+        },
+        subFromCart() {
+            if (this.cart > 0) {
+                this.cart -= 1;
+            }
+        }
     }
 })
 
